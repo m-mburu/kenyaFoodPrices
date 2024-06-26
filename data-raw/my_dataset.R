562ed4307260331f9b54f7add1182476c426a8a4
m-mburu <mmburu20@gmail.com>
2024-06-26 07:00:54 +0300

    add cron to retrieve data from hum data

--- data-raw/my_dataset.R
+++ data-raw/my_dataset.R
@@ -1,21 +1,21 @@
 ## code to prepare `my_dataset` dataset goes here
 
 
-#library(rhdx)
+library(rhdx)
 library(ggplot2)
 library(dplyr)
 library(data.table)
 
 #remotes::install_github("dickoa/rhdx")
 
-# set_rhdx_config(hdx_site = "prod")
-#
-# ke_food_prices <- pull_dataset("wfp-food-prices-for-kenya") %>%
-#   get_resource(1) %>%
-#   read_resource() %>%
-#   setDT()
+set_rhdx_config(hdx_site = "prod")
 
-ke_food_prices <- fread("data-raw/ke_food_prices.csv")
+ke_food_prices <- pull_dataset("wfp-food-prices-for-kenya") %>%
+  get_resource(1) %>%
+  read_resource() %>%
+  setDT()
+
+#ke_food_prices <- fread("data-raw/ke_food_prices.csv")
 
 usethis::use_data(ke_food_prices, overwrite = TRUE)
 
