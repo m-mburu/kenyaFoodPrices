3d3fa450009ef437c89ea24452e6c07a448561c3
GitHub Action <action@github.com>
2025-01-01 00:51:49 +0000

    automatic updates 2025-01-01

--- R/doc_ke_food_prices.R
+++ R/doc_ke_food_prices.R
@@ -2,7 +2,7 @@
 #'
 #' Description.
 #'
-#' @format A data frame with 13884 rows and 23 variables:
+#' @format A data frame with 17572 rows and 23 variables:
 #' \describe{
 #'   \item{ unique_id }{  integer }
 #'   \item{ date }{  Date }
