12f607a0a63148a0ddd3ac90464e63f5ec694d01
Moses Mburu <Moses.Mburu@Finddx.org>
2026-07-03 12:50:11 +0300

    deploy with stringr install

--- dev/deploy.R
+++ dev/deploy.R
@@ -1,12 +1,28 @@
-cran_repo <- "https://cloud.r-project.org"
-options(repos = c(CRAN = cran_repo))
-Sys.unsetenv("RSPM")
-Sys.setenv(RENV_CONFIG_REPOS_OVERRIDE = paste0("CRAN=", cran_repo))
-rsconnect::setAccountInfo(name='mmburu',
-                          token=Sys.getenv("RS_CONNECT_TOKEN"),
-                          secret= Sys.getenv("RS_CONNECT_SECRET"))
+# Building a Prod-Ready, Robust Shiny Application.
+#
+# README: each step of the dev files is optional, and you don't have to
+# fill every dev scripts before getting started.
+# 01_start.R should be filled at start.
+# 02_dev.R should be used to keep track of your development during the project.
+# 03_deploy.R should be used once you need to deploy your app.
+#
+#
+######################################
+#### CURRENT FILE: DEPLOY SCRIPT #####
+######################################
 
-#golem::add_shinyappsio_file()
+# Test your app
+
+## Run checks ----
+## Check the package before sending to prod
+install.packages("stringr")
+golem::add_shinyserver_file()
+
+
+
+
+# Deploy to Posit Connect or ShinyApps.io
+# In command line.
 rsconnect::deployApp(
   appName = desc::desc_get_field("Package"),
   appTitle = desc::desc_get_field("Package"),
@@ -21,7 +37,5 @@ rsconnect::deployApp(
   ),
   appId = rsconnect::deployments(".")$appID,
   lint = FALSE,
-  forceUpdate = TRUE,
-  packageRepositoryResolutionR = "lax",
-  dependencyResolution = "library"
+  forceUpdate = TRUE
 )
